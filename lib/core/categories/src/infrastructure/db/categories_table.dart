982a34b4b09fbdc92e2a0f345f1ba3acf11556ec
Thomas Viana <thomasviana16@gmail.com>
2021-12-31 17:34:23 -0500

    Create categories table for local storage.

---
+++ lib/core/categories/src/infrastructure/db/categories_table.dart
@@ -0,0 +1,39 @@
+import 'package:drift/drift.dart';
+
+import 'categories_db.dart';
+
+part 'categories_table.g.dart';
+
+@DataClassName('CategoryDbDto')
+class CategoryTable extends Table {
+  TextColumn get id => text().customConstraint('UNIQUE')();
+  TextColumn get userId => text().nullable()();
+  TextColumn get name => text()();
+  IntColumn get icon => integer()();
+  IntColumn get color => integer()();
+  //TODO: Subcategories
+
+  @override
+  Set<Column> get primaryKey => {id};
+
+  @override
+  String get tableName => 'category';
+}
+
+@DriftAccessor(tables: [CategoryTable])
+class CategoryDao extends DatabaseAccessor<CategoriesDatabase>
+    with _$CategoryDaoMixin {
+  CategoryDao(CategoriesDatabase db) : super(db);
+
+  Stream<List<CategoryDbDto>> getCategories(String? userId) {
+    final query = select(categoryTable)
+      ..where((tbl) => tbl.userId.equals(userId));
+    return query.watch();
+  }
+
+  Future<void> createOrUpdate(CategoryDbDto category) =>
+      into(categoryTable).insertOnConflictUpdate(category);
+
+  Future<void> deleteNote(String categoryId) =>
+      (delete(categoryTable)..where((tbl) => tbl.id.equals(categoryId))).go();
+}
---
+++ lib/core/categories/src/infrastructure/db/categories_table.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'categories_table.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$CategoryDaoMixin on DatabaseAccessor<CategoriesDatabase> {
+  $CategoryTableTable get categoryTable => attachedDatabase.categoryTable;
+}

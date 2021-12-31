46a684bae2127a84850c8a13b7683edcb1ee36ec
Thomas Viana <thomasviana16@gmail.com>
2021-12-31 17:33:28 -0500

    Create file for later category repo implementation

---
+++ lib/core/categories/src/infrastructure/category_repository_impl.dart
@@ -0,0 +1,21 @@
+import '../../domain.dart';
+
+class CategoryRepositoryImpl implements CategoryRepository {
+  @override
+  Future<void> delete(CategoryId categoryId) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<Category>> fetchCategories(CategoryUserId? userId) {
+    // TODO: implement fetchCategories
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> save(Category note) {
+    // TODO: implement save
+    throw UnimplementedError();
+  }
+}

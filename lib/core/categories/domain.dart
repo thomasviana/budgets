496551fc6604e29d8547e6ba793498daa29b9dd6
Thomas Viana <thomasviana16@gmail.com>
2022-01-05 18:02:14 -0500

    Updtae barrel files on cateogories.

--- lib/core/categories/domain.dart
+++ lib/core/categories/domain.dart
@@ -4,4 +4,5 @@ export './src/domain/category.dart';
 export './src/domain/category_model.dart';
 export './src/domain/category_respository.dart';
 export './src/domain/sub_category.dart';
+export './src/domain/sub_category_respository.dart';
 export './src/domain/value_objects.dart';

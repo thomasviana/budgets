d74442c657ba92f4fa5dc4ffb810e9df1bdaf4e2
Thomas Viana <thomasviana16@gmail.com>
2022-01-07 12:38:25 -0500

    Create accounts barrel file.

---
+++ lib/core/accounts/application.dart
@@ -0,0 +1,8 @@
+library application;
+
+export './src/application/create_account.dart';
+export './src/application/create_category.dart';
+export './src/application/delete_account.dart';
+export './src/application/get_accounts.dart';
+export './src/application/save_accounts.dart';
+export './src/application/update_account.dart';

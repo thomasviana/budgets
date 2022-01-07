8887f0dedebed4d8ac2e97bb53e74042bfc9b841
Thomas Viana <thomasviana16@gmail.com>
2022-01-07 16:22:55 -0500

    update barrel file.

--- lib/core/user/application.dart
+++ lib/core/user/application.dart
@@ -1,9 +1,9 @@
 library application;
 
 export './src/application/check_auth_status.dart';
-export './src/application/create_account.dart';
 export './src/application/get_profile_info.dart';
 export './src/application/log_out.dart';
 export './src/application/pick_user_image.dart';
 export './src/application/sign_in.dart';
 export './src/application/update_user_info.dart';
+export 'src/application/create_user.dart';

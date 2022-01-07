b41f838ed2a5c22a0a6ec9a952c34de0ebd1b0df
Thomas Viana <thomasviana16@gmail.com>
2022-01-07 12:39:30 -0500

    Create new UI resources like new colors and accopunt logos.

--- lib/presentation/resources/colors.dart
+++ lib/presentation/resources/colors.dart
@@ -32,5 +32,23 @@ class CategoryColors {
   static const int blue_grey = 0xFF607D8B;
 }
 
+class AccountColors {
+  static const int red = 0xFFF44336;
+  static const int pink = 0xFFE91E63;
+  static const int purple = 0xFFC449C2;
+  static const int deep_purple = 0xFF7C4DFF;
+  static const int indigo = 0xFF3F51B5;
+  static const int blue = 0xFF03A9F4;
+  static const int cyan = 0xFF00BCD4;
+  static const int teal = 0xFF009688;
+  static const int green = 0xFFCDDC39;
+  static const int yellow = 0xFFFFEB3B;
+  static const int amber = 0xFFFFC107;
+  static const int orange = 0xFFF57C00;
+  static const int deep_orange = 0xFFF44336;
+  static const int grey = 0xFF616161;
+  static const int blue_grey = 0xFF607D8B;
+}
+
 final currency = NumberFormat('#,##0.00', 'en_US');
 const kDefaultPadding = 20.0;

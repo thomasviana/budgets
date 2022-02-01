a9f46a7064a0da1c393463a8af1f9487f200411d
Thomas Viana <thomasviana16@gmail.com>
2022-02-01 12:48:59 -0500

    Change currency format to no decimals.

--- lib/presentation/resources/constants.dart
+++ lib/presentation/resources/constants.dart
@@ -2,5 +2,5 @@ import 'package:intl/intl.dart';
 
 const kDefaultPhotoUrl =
     'https://thumbs.dreamstime.com/b/vector-de-usuario-redes-sociales-perfil-avatar-predeterminado-retrato-vectorial-del-176194876.jpg';
-final currency = NumberFormat('#,##0.00', 'en_US');
+final currency = NumberFormat('#,##0', 'en_US');
 const kDefaultPadding = 20.0;

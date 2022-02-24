5060a28480fecb03b50e79b07c0917595c096b7c
Thomas Viana <thomasviana16@gmail.com>
2022-02-24 16:36:49 -0500

    Remove getProfile usecase on categories view model.

--- lib/presentation/screens/budgets/edit_budget_bloc/edit_budget_screen_bloc.dart
+++ lib/presentation/screens/budgets/edit_budget_bloc/edit_budget_screen_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:budgets/core/budgets/application.dart';
 import 'package:budgets/core/budgets/domain.dart';
-import 'package:budgets/core/user/application.dart';
 import 'package:injectable/injectable.dart';
 
 part 'edit_budget_screen_event.dart';
@@ -12,51 +11,48 @@ class EditBudgetScreenBloc
     extends Bloc<EditBudgetScreenEvent, EditBudgetScreenState> {
   UpdateBudget updateBudget;
   DeleteBudget deleteBudget;
-  GetProfileInfo getProfileInfo;
   CreateBudget createBudget;
   EditBudgetScreenBloc(
     this.updateBudget,
     this.deleteBudget,
-    this.getProfileInfo,
     this.createBudget,
   ) : super(EditBudgetScreenState.initial()) {
     on<CheckBudget>((event, emit) {
       event.budget != null
-          ? emit(state.copyWith(
-              budget: event.budget, isEditMode: true, isLoading: false))
-          : emit(state.copyWith(
-              budget: Budget.empty(), isEditMode: false, isLoading: false));
+          ? emit(
+              state.copyWith(
+                budget: event.budget,
+                isEditMode: true,
+                isLoading: false,
+              ),
+            )
+          : emit(
+              state.copyWith(
+                budget: Budget.empty(),
+                isEditMode: false,
+                isLoading: false,
+              ),
+            );
     });
     on<BudgetDeleted>(
       (event, emit) async => deleteBudget(state.budget!.id),
     );
-    on<BudgetSaved>(
-      (event, emit) async => getProfileInfo().then(
-        (userOption) => userOption.fold(
-          () {},
-          (user) async {
-            if (!state.isEditMode) {
-              await createBudget(
-                budgetUserId: BudgetUserId(user.id.value),
-                name: state.budget!.name,
-                color: state.budget!.color,
-                abbreviation: state.budget!.abbreviation,
-                balance: state.budget!.balance,
-              );
-            } else {
-              await updateBudget(
-                userId: BudgetUserId(user.id.value),
-                budgetId: state.budget!.id,
-                name: state.budget!.name,
-                color: state.budget!.color,
-                abbreviation: state.budget!.abbreviation,
-                balance: state.budget!.balance,
-              );
-            }
-          },
-        ),
-      ),
-    );
+    on<BudgetSaved>((event, emit) async {
+      if (!state.isEditMode) {
+        await createBudget(
+          name: state.budget!.name,
+          color: state.budget!.color,
+          abbreviation: state.budget!.abbreviation,
+        );
+      } else {
+        await updateBudget(
+          budgetId: state.budget!.id,
+          name: state.budget!.name,
+          color: state.budget!.color,
+          abbreviation: state.budget!.abbreviation,
+        );
+      }
+    });
     on<ColorUpdated>(
       (event, emit) => emit(
         state.copyWith(budget: state.budget!.copyWith(color: event.color)),
@@ -71,7 +67,10 @@ class EditBudgetScreenBloc
     on<AbbreviationChanged>(
       (event, emit) => emit(
         state.copyWith(
-            budget: state.budget!.copyWith(abbreviation: event.abbreviation)),
+          budget: state.budget!.copyWith(
+            abbreviation: event.abbreviation,
+          ),
+        ),
       ),
     );
     on<BalanceChanged>(

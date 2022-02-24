db5096cea754811f1ddd1ce7145b9bf224a91677
Thomas Viana <thomasviana16@gmail.com>
2022-02-24 16:44:25 -0500

    Update and refactor  manage income screen view model.

--- lib/presentation/screens/transactions/manage_income_bloc/manage_income_screen_bloc.dart
+++ lib/presentation/screens/transactions/manage_income_bloc/manage_income_screen_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:core';
+
 import 'package:bloc/bloc.dart';
 import 'package:budgets/core/budgets/application.dart';
 import 'package:budgets/core/budgets/domain.dart';
@@ -18,69 +20,81 @@ class ManageIncomeScreenBloc
   ) : super(ManageIncomeScreenState.initial()) {
     on<CheckInitialValues>((event, emit) {
       event.budgets != null
-          ? emit(state.copyWith(
-              budgets: event.budgets,
-              incomeAmount: event.incomeAmount,
-              pendingAmount: event.incomeAmount,
-              isLoading: false,
-            ))
-          : emit(state.copyWith(
-              budgets: [],
-              incomeAmount: 0.0,
-              isLoading: false,
-            ));
-      for (final budget in state.budgets!) {
-        state.budgetAmounts!.add(0.0);
-        state.budgetPercentages!.add(0.0);
-      }
+          ? emit(
+              state.copyWith(
+                budgets: event.budgets,
+                incomeAmount: event.incomeAmount,
+                pendingAmount: event.incomeAmount,
+                isLoading: false,
+                budgetAmounts: List<double>.filled(
+                  event.budgets!.length,
+                  0,
+                  growable: true,
+                ),
+                budgetPercentages: List<double>.filled(
+                  event.budgets!.length,
+                  0,
+                  growable: true,
+                ),
+              ),
+            )
+          : emit(
+              state.copyWith(
+                budgets: [],
+                incomeAmount: 0.0,
+                isLoading: false,
+              ),
+            );
     });
 
     on<BudgetIncremented>((event, emit) {
       state.budgetPercentages![event.index] += 0.1;
       state.budgetAmounts![event.index] =
           state.incomeAmount! * (state.budgetPercentages![event.index]);
-      updateValues();
-      emit(state.copyWith(
-        budgetAmounts: state.budgetAmounts,
-        budgetPercentages: state.budgetPercentages,
-      ));
+      updateValues(emit);
+      emit(
+        state.copyWith(
+          budgetAmounts: state.budgetAmounts,
+          budgetPercentages: state.budgetPercentages,
+        ),
+      );
     });
 
     on<BudgetDecremented>((event, emit) {
       state.budgetPercentages![event.index] -= 0.1;
       state.budgetAmounts![event.index] =
           state.incomeAmount! * (state.budgetPercentages![event.index]);
-      updateValues();
-      emit(state.copyWith(
-        budgetAmounts: state.budgetAmounts,
-        budgetPercentages: state.budgetPercentages,
-      ));
+      updateValues(emit);
+      emit(
+        state.copyWith(
+          budgetAmounts: state.budgetAmounts,
+          budgetPercentages: state.budgetPercentages,
+        ),
+      );
     });
 
     on<IncomeManaged>(
-      (event, emit) async => getProfileInfo().then(
-        (userOption) => userOption.fold(
-          () {},
-          (user) async {
-            for (var index = 0; index < state.budgets!.length; index++) {
-              await updateBudget(
-                userId: BudgetUserId(user.id.value),
-                budgetId: state.budgets![index].id,
-                balance: state.budgetAmounts![index],
-              );
-            }
-          },
-        ),
-      ),
+      (event, emit) async {
+        for (var index = 0; index < state.budgets!.length; index++) {
+          await updateBudget(
+            budgetId: state.budgets![index].id,
+            amount: state.budgetAmounts![index].round().toDouble(),
+          );
+        }
+      },
     );
   }
 
-  void updateValues() {
+  void updateValues(Emitter emit) {
     double totalManagedAmount = 0;
     for (final amount in state.budgetAmounts!) {
-      totalManagedAmount += amount;
+      totalManagedAmount += amount.round();
     }
-    state.managedAmount = totalManagedAmount;
-    state.pendingAmount = state.incomeAmount! - state.managedAmount;
+    emit(
+      state.copyWith(
+        managedAmount: totalManagedAmount,
+        pendingAmount: state.incomeAmount! - totalManagedAmount,
+      ),
+    );
   }
 }

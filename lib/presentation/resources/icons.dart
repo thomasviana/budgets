ab3799824222841898b6bdc8fb4db892ee51ae2f
Thomas Viana <thomasviana16@gmail.com>
2022-03-02 19:23:33 -0500

    Update icon date for pie_chart_outlined-

--- lib/presentation/resources/icons.dart
+++ lib/presentation/resources/icons.dart
@@ -682,7 +682,7 @@ class AppIcons {
     Icons.picture_in_picture,
     Icons.picture_in_picture_alt,
     Icons.pie_chart,
-    Icons.pie_chart_outlined,
+    Icons.pie_chart_outline,
     Icons.pin_drop,
     Icons.place,
     Icons.play_arrow,
